0e9d9c16cd02ad86d2f905951c50849cc1c99a3e
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-20 07:42:46 +0400

    generate typeText in model itself

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,11 +1,13 @@
 package model;
 
+import utils.WordGenerator;
+
 /**
  * Stores game state.
  * 
  */
 public class Model {
-    public String typeText = ""; // Text to be typed
+    public String typeText = (new WordGenerator(10)).getString(); // Text to be typed
     public Object lastIncorrectHighlight;
     /**
      * lastIncorrectHighlight:
@@ -14,7 +16,7 @@ public class Model {
      * of the `highlightChar` function in PlayScreen when
      * a character is wrongly typed.
      */
-    public int charPtr = 0; // index of character to be typed
+    public int charPtr = 0; // index of character to be typed (cursor position)
     public long startTime = -1; // time, in ms, at which player started typing
     public long gameDuration = 0; // number of seconds elapsed since first chracter was typed
     public int totalMistakes = 0; // number of times a character was wrongly typed
