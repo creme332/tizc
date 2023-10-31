f2a47274af8bc14934b7a31b1ffe3347d550f5ce
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-31 16:45:39 +0400

    create a calculator class

--- src/controller/GameOverController.java
+++ src/controller/GameOverController.java
@@ -8,9 +8,11 @@ import javax.swing.KeyStroke;
 
 import view.GameOverScreen;
 import model.Model;
+import utils.Calculator;
 
 public class GameOverController {
     private GameOverScreen gameOverScreen = new GameOverScreen();
+    private Calculator calc = new Calculator();
     private Model model;
 
     public GameOverController(Model model) {
@@ -29,44 +31,23 @@ public class GameOverController {
     }
 
     /**
-     * Calculates the typing speed in WPM (words per minute) of a player
-     * 
-     * @param charCount total number of characters (including spaces) in text typed
-     * @param timeTaken time taken in seconds by player
-     * @return WPM
-     */
-    private long getWPM(long charCount, long timeTaken) {
-        long secondsInMinute = 60;
-        long charPerWord = 5; // average number of characters in a word
-        if (timeTaken > 0) {
-            return (secondsInMinute * charCount / (charPerWord * timeTaken));
-        }
-        return 0;
-    }
-
-    /**
-     * Calculates the accuracy of a player
-     * 
-     * @param charCount    total number of characters (including spaces) in text
-     *                     typed
-     * @param mistakeCount number of characters wrongly typed
-     * @return A percentage (0-100)
-     */
-    private long getAccuracy(long charCount, long mistakeCount) {
-        if (charCount > 0)
-            return 100 * (charCount - mistakeCount) / charCount;
-        return 0;
-
-    }
-
-    /**
      * Display game statistics: game duration, WPM, accuracy
      */
     public void showStats() {
-        gameOverScreen.setTimeTaken(model.getGameDuration()); // display game duration
-        gameOverScreen.setWPM(getWPM(model.getTypeText().length(), model.getGameDuration())); // display wpm
-        gameOverScreen.setAccuracy(getAccuracy(model.getTypeText().length(), model.getTotalMistakes())); // display
-                                                                                                         // accuracy
+        // display game duration
+        gameOverScreen.setTimeTaken(model.getGameDuration());
+
+        // display wpm
+        gameOverScreen.setWPM(
+                calc.getWPM(
+                        model.getTypeText().length(),
+                        model.getGameDuration()));
+
+        // display accuracy
+        gameOverScreen.setAccuracy(
+                calc.getAccuracy(
+                        model.getTypeText().length(),
+                        model.getTotalMistakes()));
     }
 
     public GameOverScreen getGameOverScreen() {
---
+++ src/utils/Calculator.java
@@ -0,0 +1,38 @@
+package utils;
+
+/**
+ * Calculates game statistics
+ */
+public class Calculator {
+
+    /**
+     * Calculates the typing speed in WPM (words per minute) of a player
+     * 
+     * @param charCount total number of characters (including spaces) in text typed
+     * @param timeTaken time taken in seconds by player
+     * @return WPM
+     */
+    public long getWPM(long charCount, long timeTaken) {
+        long secondsInMinute = 60;
+        long charPerWord = 5; // average number of characters in a word
+        if (timeTaken > 0) {
+            return (secondsInMinute * charCount / (charPerWord * timeTaken));
+        }
+        return 0;
+    }
+
+    /**
+     * Calculates the accuracy of a player
+     * 
+     * @param charCount    total number of characters (including spaces) in text
+     *                     typed
+     * @param mistakeCount number of characters wrongly typed
+     * @return A percentage (0-100)
+     */
+    public long getAccuracy(long charCount, long mistakeCount) {
+        if (charCount > 0)
+            return 100 * (charCount - mistakeCount) / charCount;
+        return 0;
+    }
+
+}
