0b4a84e8c33e5a5766b9ebaf1be88b149345825b
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-28 16:28:23 +0400

    add flatlaf

--- src/main/java/com/github/creme332/App.java
+++ src/main/java/com/github/creme332/App.java
@@ -1,9 +1,18 @@
 package com.github.creme332;
 
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatDarkLaf;
 import com.github.creme332.controller.Controller;
 
 public class App {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+
         new Controller();
     }
 }
