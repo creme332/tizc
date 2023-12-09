e167c6baaae4f7a4d2b144cd79181fe6f22ba11b
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-09 13:26:01 +0400

    add validation checks

--- src/main/java/com/github/creme332/utils/IconLoader.java
+++ src/main/java/com/github/creme332/utils/IconLoader.java
@@ -13,11 +13,20 @@ public class IconLoader {
      * @param iconSize size of icon in pixels
      * @return
      */
-    public ImageIcon loadIcon(String path, int iconSize) {
-        return new ImageIcon(
-                new ImageIcon(this.getClass().getResource(path)).getImage().getScaledInstance(iconSize,
-                        iconSize,
-                        Image.SCALE_DEFAULT));
+    public ImageIcon loadIcon(String path, int iconSize) throws Exception {
+        if (iconSize < 1) {
+            throw new Exception("Icon size must be a positive integer");
+        }
+
+        try {
+            ImageIcon icon = loadIcon(path);
+            return new ImageIcon(
+                    icon.getImage().getScaledInstance(iconSize,
+                            iconSize,
+                            Image.SCALE_DEFAULT));
+        } catch (Exception e) {
+            throw (e);
+        }
     }
 
     /**
@@ -26,7 +35,10 @@ public class IconLoader {
      * @param path Must start with / since path is relative to resources folder.
      * @return
      */
-    public ImageIcon loadIcon(String path) {
+    public ImageIcon loadIcon(String path) throws Exception {
+        if (path.length() < 1 || path.charAt(0) != '/') {
+            throw new Exception("Path should start with /");
+        }
         return new ImageIcon(this.getClass().getResource(path));
     }
 
