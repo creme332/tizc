d55d0ac043e7d21cd7a1b1a62ea801a86dd003ea
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-11 19:18:05 +0400

    revamp settings ui

--- src/main/java/com/github/creme332/view/Settings/Form.java
+++ src/main/java/com/github/creme332/view/Settings/Form.java
@@ -1,74 +1,78 @@
 package com.github.creme332.view.Settings;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Map;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
 
+import com.github.creme332.model.Setting;
 import com.github.creme332.utils.SettingsManager;
 
 public class Form extends JPanel implements ActionListener {
     public static String name = "settingsScreen";
     private JButton exitButton = new JButton("Exit");
 
-    private Map<String, String[]> gameSettings = new SettingsManager().getSettings();
-    private GridBagLayout layout;
+    private Map<String, Setting> gameSettings = new SettingsManager().getSettings();
 
     public Form() {
-        GridBagConstraints gbc = new GridBagConstraints();
-        layout = new GridBagLayout();
-        this.setLayout(layout);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        // set global gbc constraints
-        gbc.weightx = 1;
-        gbc.weighty = 1;
-        gbc.gridwidth = 1;
+        JPanel titlePanel = new JPanel();
+        JLabel title = new JLabel("Settings");
+        title.putClientProperty("FlatLaf.style", "font: $h1.font");
+        titlePanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        titlePanel.add(title);
+        this.add(titlePanel);
 
-        int row = 0;
-        int column = 1;
+        JPanel sectionsContainer = new JPanel();
+        sectionsContainer.setLayout(new BoxLayout(sectionsContainer, BoxLayout.Y_AXIS));
+
         for (String settingName : gameSettings.keySet()) {
-            Section section = new Section(settingName, gameSettings.get(settingName));
-            gbc.gridx = column;
-            gbc.gridy = row;
-            gbc.gridwidth = 2;
-            this.add(section, gbc);
-            row++;
+            Section section = new Section(gameSettings.get(settingName));
+            sectionsContainer.add(section);
         }
+
+        JScrollPane scrollPane = new JScrollPane(sectionsContainer);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+
+        this.add(scrollPane);
 
         // add exit button
-        gbc.gridx = column;
-        gbc.gridy = row;
-        gbc.gridwidth = 1;
-        exitButton.setPreferredSize(new Dimension(100, 100));
+        JPanel exitPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        exitPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        exitButton.setPreferredSize(new Dimension(100, 60));
         exitButton.putClientProperty("FlatLaf.style", "font: bold 150% $defaultFont");
-        this.add(exitButton, gbc);
+        exitPanel.add(exitButton);
+
+        this.add(exitPanel);
     }
 
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        int[][] dims = layout.getLayoutDimensions();
-        g.setColor(Color.white);
-        int x = 0;
-        for (int add : dims[0]) {
-            x += add;
-            g.drawLine(x, 0, x, getHeight());
-        }
-        int y = 0;
-        for (int add : dims[1]) {
-            y += add;
-            g.drawLine(0, y, getWidth(), y);
-        }
-    }
+    // @Override
+    // public void paint(Graphics g) {
+    // super.paint(g);
+    // int[][] dims = layout.getLayoutDimensions();
+    // g.setColor(Color.white);
+    // int x = 0;
+    // for (int add : dims[0]) {
+    // x += add;
+    // g.drawLine(x, 0, x, getHeight());
+    // }
+    // int y = 0;
+    // for (int add : dims[1]) {
+    // y += add;
+    // g.drawLine(0, y, getWidth(), y);
+    // }
+    // }
 
     public void actionPerformed(ActionEvent e) {
         System.out.println(e.getActionCommand());
--- src/main/java/com/github/creme332/view/Settings/Section.java
+++ src/main/java/com/github/creme332/view/Settings/Section.java
@@ -1,31 +1,60 @@
 package com.github.creme332.view.Settings;
 
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.UIManager;
 
+import com.github.creme332.model.Setting;
 import com.github.creme332.utils.SettingsManager;
 
 public class Section extends JPanel implements ActionListener {
     private String name;
     private String unknownPreference = "Unknown";
-
     private SettingsManager settings = new SettingsManager();
 
-    public Section(String settingName, String[] options) {
-        this.name = settingName;
+    public Section(Setting setting) {
+        this.name = setting.getKey();
+        String[] options = setting.getOptions();
 
-        JLabel title = new JLabel(settingName);
+        // position children of JPanel vertically
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        // create children of Section
+        JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+        JPanel bodyPanel = new JPanel(new GridLayout(1, 2));
+
+        // create children of bodyPanel
+        JPanel descriptionPanel = new JPanel();
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        // create title
+        JLabel title = new JLabel(name);
         title.putClientProperty("FlatLaf.style", "font: bold $h2.regular.font");
-        this.add(title);
+        titlePanel.add(title);
+        this.add(titlePanel);
+
+        // create description
+        JTextArea descriptionLabel = new JTextArea(3, 40);
+        descriptionLabel.setText(setting.getDescription());
+        descriptionLabel.setEditable(false);
+        descriptionLabel.setLineWrap(true);
+        descriptionLabel.setWrapStyleWord(true);
+        descriptionLabel.setOpaque(false); // Make transparent
+
+        descriptionPanel.add(descriptionLabel);
+        bodyPanel.add(descriptionPanel);
 
         ButtonGroup group = new ButtonGroup();
-        String currentPreference = settings.getData(settingName);
+        String currentPreference = settings.getData(name);
 
         // if no preference set, use first option as default preference
         if (currentPreference.equals(unknownPreference)) {
@@ -39,10 +68,13 @@ public class Section extends JPanel implements ActionListener {
             if (currentPreference.equals(options[i]))
                 btn.setSelected(true);
             group.add(btn);
-            this.add(btn);
+            buttonsPanel.add(btn);
             btn.addActionListener(this); // Register a listener for the radio buttons.
 
         }
+
+        bodyPanel.add(buttonsPanel);
+        this.add(bodyPanel);
 
         this.setOpaque(false); // make section transparent
     }
